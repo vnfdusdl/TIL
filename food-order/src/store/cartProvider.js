184e3d1322b4fa0ad0848580fefd7f7e2a42bc26
choyeon <rlachdus2@naver.com>
2022-09-14 22:57:10 +0900

    [study] 장바구니 리듀서 추가

--- food-order/src/store/cartProvider.js
+++ food-order/src/store/cartProvider.js
@@ -1,14 +1,39 @@
 // 이 컴포넌트는 CartContext 데이터를 관리하고 이 컨텍스트를 다른 컴포넌트에게 제공하는 것.
+import { useReducer } from 'react';
+
 import CartContext from './cartContext';
 
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  f(action.type === 'ADD') {
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+
+  return;
+};
+
 const CartProvider = (props) => {
-  const addItemToCartHandler = (item) => {};
+  const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+
+  const addItemToCartHandler = (item) => {
+      dispatchCartAction({ type: 'ADD', item });
+    
+  };
 
   const removeItemFromCartHandler = (id) => {};
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };

f6dae30e6bb4b68ac8fc901bd07c276ef199c5e2
choyeon <rlachdus2@naver.com>
2022-09-20 19:49:49 +0900

    [study] 장바구니에 중복 아이템 추가시 수량이 변경되도록 변경 - 기존에는 새로운 아이템 항목으로 추가되었음.

--- food-order/src/store/cartProvider.js
+++ food-order/src/store/cartProvider.js
@@ -10,7 +10,27 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === 'ADD') {
-    const updatedItems = state.items.concat(action.item);
+    // 추가하려는 아이템이 이미 존재한다면 index를 반환
+    const existingCartItemIndex = state.items.findIndex((item) => item.id === action.item.id);
+    // 추가하려는 아이템과 같은 아이템
+    const existingCartItem = state.items[existingCartItemIndex];
+    let updatedItems;
+    // 아이템이 존재한다면,
+    if (existingCartItem) {
+      //그 아이템이 업데이트 될 것. 기존의 상태에서, amount만 추가
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      //새로 업데이트 되는 아이템 리스트의 복사본
+      updatedItems = [...state.items];
+      //추가하는 아이템만 바꿔줌
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      //존재하지 않는 경우라면 단순하게 리스트에 추가해주기만 하면 됨
+      updatedItems = state.items.concat(action.item);
+    }
+
     const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
 
     return {

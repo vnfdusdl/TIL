e1032c6e9ccf915c83a1f599bd48460c58c14744
choyeon <rlachdus2@naver.com>
2022-09-21 10:38:03 +0900

    [study] sass 사용 익히기

---
+++ styling-react/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SassComponent from './sass/SassComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <SassComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ styling-react/src/sass/SassComponent.js
@@ -0,0 +1,13 @@
+import './src/SassComponent.scss';
+
+const SassComponent = () => {
+    return(
+        <div className='SassComponent'>
+            <div className='box red'></div>
+            <div className='box orange'></div>
+            <div className='box yellow'></div>
+        </div>
+    )
+}
+
+export default SassComponent;

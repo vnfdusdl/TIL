a52cb442645e835a79cf55516892924e19036813
choyeon <rlachdus2@naver.com>
2022-09-22 17:44:42 +0900

    [study] React.memo 를 이용한 최적화

--- behind-the-scenses/src/components/Demo/DemoOutput.js
+++ behind-the-scenses/src/components/Demo/DemoOutput.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const DemoOutput = (props) => {
     console.log('DemoOutput RUNNING');
+    useEffect(() => {
+        console.log('useEffect 실행')
+    } ,[props.show])
   return <p>{props.show ? 'This is new!' : ''}</p>;
 };
 
-export default DemoOutput;
+export default React.memo(DemoOutput);
--- behind-the-scenses/src/components/UI/Button/Button.js
+++ behind-the-scenses/src/components/UI/Button/Button.js
@@ -3,17 +3,16 @@ import React from 'react';
 import classes from './Button.module.css';
 
 const Button = (props) => {
-  console.log('Button RUNNING')
+  console.log('Button RUNNING');
   return (
     <button
       type={props.type || 'button'}
       className={`${classes.button} ${props.className}`}
       onClick={props.onClick}
-      disabled={props.disabled}
-    >
+      disabled={props.disabled}>
       {props.children}
     </button>
   );
 };
 
-export default Button;
+export default React.memo(Button);

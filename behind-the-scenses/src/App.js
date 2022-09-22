28aa2aad3f756e9c811681e6233f37ae1a3525d9
choyeon <rlachdus2@naver.com>
2022-09-22 16:37:48 +0900

    [study] 자식 컴포넌트 재평가 - 부모 컴포넌트가 재평가되면 자식 컴포넌트도 재평가 됨 - 재평가가 DOM 의 변경으로 이어지는 것은 아님

--- behind-the-scenses/src/App.js
+++ behind-the-scenses/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 import './App.css';
+import DemoOutput from './components/Demo/DemoOutput';
 import Button from './components/UI/Button/Button';
 
 function App() {
@@ -15,7 +16,7 @@ function App() {
   return (
     <div className='app'>
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is new!</p>}
+      <DemoOutput show={false}/>
       <Button onClick={toggleParagraph}>show</Button>
     </div>
   );

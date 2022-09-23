6c946399474dc997af7a4b2a924fa1eeacde29ad
choyeon <rlachdus2@naver.com>
2022-09-24 08:29:00 +0900

    [study] useParams  훅으로 URL 파라미터 이용

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 
 import Home from './pages/Home';
 import About from './pages/About';
+import Profile from './pages/Profile';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/about' element={<About />} />
+      <Route path='/profile/:username' element={<Profile />} />
     </Routes>
   );
 }
--- router-tutorial/src/pages/Home.js
+++ router-tutorial/src/pages/Home.js
@@ -1,13 +1,23 @@
 import { Link } from 'react-router-dom';
 
 const Home = () => {
-    return (
-        <div>
-            <h1>홈</h1>
-            <p>가장 먼저 보여지는 페이지입니다.</p>
-            <Link to='/about'>소개로 이동</Link>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>홈</h1>
+      <p>가장 먼저 보여지는 페이지입니다.</p>
+      <ul>
+        <li>
+          <Link to='/about'>소개로 이동</Link>
+        </li>
+        <li>
+          <Link to='/profile/velopert'>velopert</Link>
+        </li>
+        <li>
+          <Link to='/profile/gildong'>gildong</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default Home;

092a37ec55e5170926cb399261f014ff14d3fa8d
choyeon <rlachdus2@naver.com>
2022-09-24 09:48:03 +0900

    [study] NavLink

--- router-tutorial/src/pages/Articles.js
+++ router-tutorial/src/pages/Articles.js
@@ -1,19 +1,30 @@
-import { Link, Outlet } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 
 const Articles = () => {
+  const activeStyle = {
+    color: 'green',
+    fontSize: 21,
+  };
+
   return (
     <div>
       {/* Oulet이 사용된 자리에 중첩된 라이트가 보여지게 된다.  */}
       <Outlet />
       <ul>
         <li>
-          <Link to='/articles/1'>게시글1</Link>
+          <NavLink to='/articles/1' style={({ isActive }) => (isActive ? activeStyle : undefined)}>
+            게시글1
+          </NavLink>
         </li>
         <li>
-          <Link to='/articles/2'>게시글2</Link>
+          <NavLink to='/articles/2' style={({ isActive }) => (isActive ? activeStyle : undefined)}>
+            게시글2
+          </NavLink>
         </li>
         <li>
-          <Link to='/articles/3'>게시글3</Link>
+          <NavLink to='/articles/3' style={({ isActive }) => (isActive ? activeStyle : undefined)}>
+            게시글3
+          </NavLink>
         </li>
       </ul>
     </div>

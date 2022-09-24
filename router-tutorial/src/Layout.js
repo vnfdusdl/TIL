9ac91d9f0e82df2b18ffbbebd1401d85ea1c59e5
choyeon <rlachdus2@naver.com>
2022-09-24 09:43:16 +0900

    [study] useNavigate

--- router-tutorial/src/Layout.js
+++ router-tutorial/src/Layout.js
@@ -1,9 +1,22 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 
 const Layout = () => {
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  };
+
+  const goArticle = () => {
+    navigate('/articles', { replace: true });
+  };
+
   return (
     <div>
-      <header style={{ background: 'lightgray', padding: 16, fontSize: 24 }}>Header</header>
+      <header style={{ background: 'lightgray', padding: 16, fontSize: 24 }}>
+        <button onClick={goBack}>뒤로 가기</button>
+        <button onClick={goArticle}>게시글 목록</button>
+      </header>
       <main>
         {/* 페이지 컴포넌트가 보여져야 하는 부분에 Outlet 컴포넌트 사용 */}
         <Outlet />

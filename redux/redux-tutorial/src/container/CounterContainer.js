7346aa2faeabfe208f0eddbd10b3a88766ca5412
choyeon <rlachdus2@naver.com>
2022-09-28 15:57:51 +0900

    [study] connect 함수 사용하는 여러가지 방법

--- redux/redux-tutorial/src/container/CounterContainer.js
+++ redux/redux-tutorial/src/container/CounterContainer.js
@@ -10,14 +10,12 @@ const CounterContainer = ({ number, increase, decrease }) => {
   );
 };
 
-// 리덕스 스토어 안의 상태를 컴포넌트의 props로 넘겨주기 위해 설정하는 함수
-const mapStateToProps = (state) => ({
-  number: state.counter.number,
-});
-const mapDispathToProps = (dispatch) => ({
-  // 임시함수
-  increase: () => dispatch(increase()),
-  decrease: () => dispatch(decrease()),
-});
-
-export default connect(mapStateToProps, mapDispathToProps)(CounterContainer);
+export default connect(
+  (state) => ({
+    number: state.counter.number,
+  }),
+  (dispatch) => ({
+    increase: () => dispatch(increase()),
+    decrease: () => dispatch(decrease()),
+  }),
+)(CounterContainer);

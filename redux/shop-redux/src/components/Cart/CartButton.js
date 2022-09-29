de5952451c9fc1f1556fea45e0cb50e1abd3c02a
choyeon <rlachdus2@naver.com>
2022-09-29 17:38:49 +0900

    [study] cart 모달 toggle 기능

--- redux/shop-redux/src/components/Cart/CartButton.js
+++ redux/shop-redux/src/components/Cart/CartButton.js
@@ -1,11 +1,20 @@
+import { useDispatch} from 'react-redux';
+
 import classes from './CartButton.module.css';
+import { uiActions } from '../../store/ui';
+import { Fragment } from 'react';
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+  const onClickHandler = () => {
+    dispatch(uiActions.toggle());
+  };
+
   return (
-    <button className={classes.button}>
-      <span>My Cart</span>
-      <span className={classes.badge}>1</span>
-    </button>
+      <button className={classes.button} onClick={onClickHandler}>
+        <span>My Cart</span>
+        <span className={classes.badge}>1</span>
+      </button>
   );
 };
 

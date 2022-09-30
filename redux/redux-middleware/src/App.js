6555bfeda38c12cd54639f5c3b965d967600af59
choyeon <rlachdus2@naver.com>
2022-09-30 16:46:54 +0900

    [study] redux middleware 프로젝트 시작

---
+++ redux/redux-middleware/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CounterContainer from './Container/CounterContainer';
+
+function App() {
+  return (
+    <div className='App'>
+      <CounterContainer />
+    </div>
+  );
+}
+
+export default App;

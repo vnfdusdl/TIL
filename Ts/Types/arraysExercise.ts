d4e714c7f153f65c4e4e04e5b029ce3f2ac7ef52
choyeon <rlachdus2@naver.com>
2022-10-13 12:37:03 +0900

    ts array type

---
+++ Ts/Types/arraysExercise.ts
@@ -0,0 +1,31 @@
+// **********************************************
+// ******************* PART 1 *******************
+// **********************************************
+// Create an empty array of numbers called "ages":
+const ages: number[] = [];
+// **********************************************
+// ******************* PART 2 *******************
+// **********************************************
+// Create an array variable called gameBoard that starts as an empty array.
+// It should be typed to hold a 2 dimensional array of strings
+const gameBoard: string[][] = [];
+// **********************************************
+// ******************* PART 3 *******************
+// **********************************************
+// Create a Product type that contains a name and a price.
+// An example product could be:
+// {name: "coffee mug", price: 11.50}
+type Product = {
+  name: string;
+  price: number;
+};
+// **********************************************
+// ******************* PART 4 *******************
+// **********************************************
+// Write a function called getTotal that accepts an array of Product types
+// It should return the sum of all the products' prices
+function getTotal(product: Product[]): number {
+  let sum = 0;
+  product.map((it) => (sum += it.price));
+  return sum;
+}
---
+++ Ts/arrays.ts
@@ -0,0 +1,22 @@
+const activeUsers: string[] = [];
+activeUsers.push('Tony');
+
+const ageList: number[] = [12, 45, 65];
+ageList[0] = 92;
+
+// const bools: Array<boolean> = [];
+const bools: boolean[] = [];
+
+type XY = {
+  x: number;
+  y: number;
+};
+
+const coords: XY[] = [];
+coords.push({ x: 23, y: 45 });
+
+// 다차원 배열
+const arrays: string[][] = [
+  ['a', 'b', 'c'],
+  ['d', 'e', 'f'],
+];

a0c0ac374d99fa4b31aae927828c86ffea9ce2f3
choyeon <rlachdus2@naver.com>
2022-10-22 09:53:15 +0900

    mainPage에 LoginPage로 이동하는 button 추가

--- oauth-practice/src/pages/Main.tsx
+++ oauth-practice/src/pages/Main.tsx
@@ -1,5 +1,24 @@
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
 const MainPage = () => {
-  return <></>;
+  const navigate = useNavigate();
+  const onClickHandler = () => {
+    navigate('/login');
+  };
+
+  return (
+    <div>
+      <Button onClick={onClickHandler}>로그인페이지 가기</Button>
+    </div>
+  );
 };
 
 export default MainPage;
+
+const Button = styled.button`
+  width: 200px;
+  padding: 20px 10px;
+  border: none;
+  background-color: skyblue;
+`
